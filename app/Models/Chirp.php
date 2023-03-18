b6e430c9497265a51d4bc505a756dc0eeaa014c4
Somanyu <somanyu.03samal@gmail.com>
2023-03-18 11:04:42 +0530

    💄 styles: showing chirps

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -12,4 +13,9 @@ class Chirp extends Model
     protected $fillable = [
         'message',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
